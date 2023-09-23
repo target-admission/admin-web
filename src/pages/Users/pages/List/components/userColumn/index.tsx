cbace26c67663195f9f05a6d294a9d91aaeb39a8
tam11a <ibrahimsadiktamim@gmail.com>
2023-09-23 12:18:18 +0600

    done

--- src/pages/Users/pages/List/components/userColumn/index.tsx
+++ src/pages/Users/pages/List/components/userColumn/index.tsx
@@ -4,7 +4,75 @@ import { Button } from "antd";
 import moment from "moment";
 import { Link } from "react-router-dom";
 
+import { useDeleteUser, useSuspendUser } from "@/queries/users";
+import { IUserId } from "@/types";
+import handleResponse from "@/utilities/handleResponse";
+import { message } from "@components/antd/message";
+
 const UserColumn = (): GridColDef[] => {
+	const { mutateAsync: Delete, isLoading: isDeleteLoading } = useDeleteUser();
+
+	const onDelete = async (
+		id: IUserId,
+		permanent: any = null,
+		restore: any = null
+	) => {
+		message.open({
+			type: "loading",
+			content: permanent
+				? "Deleting User Permanently.."
+				: restore
+				? "Restoring User.."
+				: "Deleting User..",
+			duration: 0,
+		});
+		const res = await handleResponse(() =>
+			Delete({
+				id,
+				params: {
+					permanent,
+					restore,
+				},
+			})
+		);
+
+		message.destroy();
+
+		if (res.status) {
+			message.success(res.message);
+			return true;
+		} else {
+			message.error(res.message);
+			return false;
+		}
+	};
+
+	const { mutateAsync: Suspend, isLoading: isSuspendLoading } =
+		useSuspendUser();
+
+	const onSuspend = async (id: IUserId) => {
+		message.open({
+			type: "loading",
+			content: "Suspending User..",
+			duration: 0,
+		});
+		const res = await handleResponse(() =>
+			Suspend({
+				id,
+			})
+		);
+
+		message.destroy();
+
+		if (res.status) {
+			message.success(res.message);
+			return true;
+		} else {
+			message.error(res.message);
+			return false;
+		}
+	};
+
 	return [
 		{
 			headerName: "ID",
@@ -134,14 +202,46 @@ const UserColumn = (): GridColDef[] => {
 				/>,
 				<GridActionsCellItem
 					icon={
+						params.row.is_active ? (
+							<Iconify
+								icon={"lucide:shield-ban"}
+								className="text-lg"
+							/>
+						) : (
+							<Iconify
+								icon={"tabler:shield-up"}
+								className="text-lg"
+							/>
+						)
+					}
+					disabled={isSuspendLoading}
+					showInMenu
+					label={params.row.is_active ? "Suspend User" : "Activate User"}
+					onClick={() => onSuspend(params.id)}
+				/>,
+				<GridActionsCellItem
+					icon={
 						<Iconify
 							icon={"icon-park-twotone:delete"}
 							className="text-lg"
 						/>
 					}
+					disabled={isDeleteLoading}
 					showInMenu
 					label="Delete"
-					onClick={() => {}}
+					onClick={() => onDelete(params.id)}
+				/>,
+				<GridActionsCellItem
+					icon={
+						<Iconify
+							icon={"icon-park-twotone:delete-five"}
+							className="text-lg"
+						/>
+					}
+					disabled={isDeleteLoading}
+					showInMenu
+					label="Permanently Delete"
+					onClick={() => onDelete(params.id, true)}
 				/>,
 			],
 		},
