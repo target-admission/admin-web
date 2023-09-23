3f50dd695115f7c85b3057a44ac5e6e502743aee
tam11a <ibrahimsadiktamim@gmail.com>
2023-09-23 11:54:33 +0600

    updated: shits

--- src/pages/Users/pages/Info/pages/Settings/index.tsx
+++ src/pages/Users/pages/Info/pages/Settings/index.tsx
@@ -1,60 +1,108 @@
 import React from "react";
-import { Input, Divider } from "antd";
+import { Input, Divider, Button as AntButton, Spin, Popconfirm } from "antd";
 import {
-  Controller,
-  //  FieldValues,
-  useForm,
+	Controller,
+	//  FieldValues,
+	useForm,
 } from "react-hook-form";
 import { Button } from "@mui/material";
 import Label from "@components/Label";
-// import { Icon } from "@iconify/react";
-// import useAuth from "@/hooks/useAuth";
-// import { message } from "@components/antd/message";
-// import { useUpdatePassword } from "@/queries/auth";
-// import handleResponse from "@/utilities/handleResponse";
+import { useParams } from "react-router-dom";
+import { message } from "@components/antd/message";
+import handleResponse from "@/utilities/handleResponse";
+import {
+	useDeleteUser,
+	useGetUsersById,
+	useSuspendUser,
+} from "@/queries/users";
 
 const Security: React.FC = () => {
-  const {
-    // reset, handleSubmit,
-    control,
-  } = useForm({
-    // resolver: joiResolver(loginResolver),
-  });
-  // const { mutateAsync: updatePassword, isLoading: isSubmitting } =
-  //   useUpdatePassword();
+	const {
+		control,
+		formState: { isDirty },
+	} = useForm({});
 
-  // const onValid = async (d: FieldValues) => {
-  //   messageApi.open({
-  //     type: "loading",
-  //     content: `Creating new password...`,
-  //     duration: 0,
-  //   });
-  //   const res = await handleResponse(() => updatePassword({ ...d }), [200]);
-  //   messageApi.destroy();
-  //   if (res.status) {
-  //     messageApi.success("Password updated successfully!");
-  //     reset();
-  //   } else messageApi.error(res.message);
-  // };
-  return (
-    <>
-      <div className="container max-w-xl mx-auto">
-        <p className="pt-5 text-xl">Change Password</p>
-        <form
-          className="py-2"
-          // onSubmit={handleSubmit(onValid)}
-        >
-          <Controller
-            control={control}
-            name={"new_password"}
-            render={({
-              field: { onChange, onBlur, value },
-              fieldState: { error },
-            }) => (
-              <>
-                <Label className="my-2">
-                  New Password
-                  {/* {error ? (
+	const { id } = useParams<{ id: string }>();
+
+	const { data, isLoading } = useGetUsersById(id);
+
+	const { mutateAsync: Delete, isLoading: isDeleteLoading } = useDeleteUser();
+
+	const onDelete = async (permanent: any = null, restore: any = null) => {
+		message.open({
+			type: "loading",
+			content: permanent
+				? "Deleting Employee Permanently.."
+				: restore
+				? "Restoring Employee.."
+				: "Deleting Employee..",
+			duration: 0,
+		});
+		const res = await handleResponse(() =>
+			Delete({
+				id,
+				params: {
+					permanent,
+					restore,
+				},
+			})
+		);
+
+		message.destroy();
+
+		if (res.status) {
+			message.success(res.message);
+			return true;
+		} else {
+			message.error(res.message);
+			return false;
+		}
+	};
+
+	const { mutateAsync: Suspend, isLoading: isSuspendLoading } =
+		useSuspendUser();
+
+	const onSuspend = async () => {
+		message.open({
+			type: "loading",
+			content: "Suspending User..",
+			duration: 0,
+		});
+		const res = await handleResponse(() =>
+			Suspend({
+				id,
+			})
+		);
+
+		message.destroy();
+
+		if (res.status) {
+			message.success(res.message);
+			return true;
+		} else {
+			message.error(res.message);
+			return false;
+		}
+	};
+
+	console.log(data);
+
+	return (
+		<Spin spinning={isLoading}>
+			<div className="container max-w-xl mx-auto">
+				<p className="pt-5 text-xl">Change Password</p>
+				<form className="my-2 p-3 border border-slate-200 rounded-md">
+					<Controller
+						control={control}
+						name={"new_password"}
+						render={({
+							field: { onChange, onBlur, value },
+							fieldState: { error },
+						}) => (
+							<>
+								<Label className="mb-2">
+									New Password
+									{/* {error ? (
                     <ErrorSuffix error={error} />
                   ) : (
                     <Tooltip
@@ -66,30 +114,31 @@ const Security: React.FC = () => {
                       </Icon> 
                     </Tooltip>
                   )}  */}
-                </Label>
-                <Input.Password
-                  placeholder="New Password"
-                  size="large"
-                  onChange={onChange}
-                  onBlur={onBlur}
-                  value={value}
-                  status={error ? "error" : ""}
-                />
-              </>
-            )}
-          />
+								</Label>
+								<Input.Password
+									readOnly
+									placeholder="New Password"
+									size="large"
+									onChange={onChange}
+									onBlur={onBlur}
+									value={value}
+									status={error ? "error" : ""}
+								/>
+							</>
+						)}
+					/>
 
-          <Controller
-            control={control}
-            name={"cpassword"}
-            render={({
-              field: { onChange, onBlur, value },
-              fieldState: { error },
-            }) => (
-              <>
-                <Label className="my-2">
-                  Confirm New Password
-                  {/* {error ? (
+					<Controller
+						control={control}
+						name={"cpassword"}
+						render={({
+							field: { onChange, onBlur, value },
+							fieldState: { error },
+						}) => (
+							<>
+								<Label className="my-2">
+									Confirm New Password
+									{/* {error ? (
                     <ErrorSuffix error={error} />
                   ) : (
                     <Tooltip
@@ -99,105 +148,218 @@ const Security: React.FC = () => {
                       <Icon icon="ph:info-fill" />
                     </Tooltip>
                   )} */}
-                </Label>
-                <Input.Password
-                  placeholder="Confirm New Password"
-                  size="large"
-                  onChange={onChange}
-                  onBlur={onBlur}
-                  value={value}
-                  status={error ? "error" : ""}
-                />
-              </>
-            )}
-          />
-          <Button
-            size="large"
-            variant="contained"
-            fullWidth
-            className="mt-5 bg-slate-600"
-            type="submit"
-            // disabled={isSubmitting}
-          >
-            Create New Password
-          </Button>
-        </form>
-        <Divider />
-        <p className="py-2 text-xl">Danger Zone</p>
-        <div className="grid grid-cols-3 border border-primary p-3 rounded-md gap-4 ">
-          <div className="col-span-2">
-            <p className=" text-md font-semibold">Suspend User</p>
-            <p className="text-xs">
-              Suspending a user involves temporarily restricting their access to
-              an online platform.
-            </p>
-          </div>
-          <div>
-            <Button
-              variant="contained"
-              className="bg-slate-600 float-right"
-              type="submit"
-              // disabled={isSubmitting}
-            >
-              Suspend
-            </Button>
-          </div>
-          <div className="col-span-2">
-            <p className=" text-md font-semibold">Delete User</p>
-            <p className="text-xs">
-              Deleting a user involves temporarily removing their account and
-              associated data.
-            </p>
-          </div>
-          <div>
-            <Button
-              variant="contained"
-              className="bg-slate-600 float-right"
-              type="submit"
-              // disabled={isSubmitting}
-            >
-              Delete
-            </Button>
-          </div>
-          <div className="col-span-2">
-            <p className=" text-md font-semibold">Delete User Permanently</p>
-            <p className="text-xs">
-              Deleting a user involves permanently removing their account and
-              associated data. You will not be able to recover this user.
-            </p>
-          </div>
-          <div>
-            <Button
-              variant="contained"
-              className="bg-slate-600 float-right"
-              type="submit"
-              // disabled={isSubmitting}
-            >
-              Delete
-            </Button>
-          </div>
-          <div className="col-span-2">
-            <p className=" text-md font-semibold">Restore User</p>
-            <p className="text-xs">
-              Restoring a user involves reinstating their previously deleted or
-              suspended account, and allowing them to regain access and
-              functionality."
-            </p>
-          </div>
-          <div>
-            <Button
-              variant="contained"
-              className="bg-slate-600 float-right"
-              type="submit"
-              // disabled={isSubmitting}
-            >
-              Restore
-            </Button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+								</Label>
+								<Input.Password
+									readOnly
+									placeholder="Confirm New Password"
+									size="large"
+									onChange={onChange}
+									onBlur={onBlur}
+									value={value}
+									status={error ? "error" : ""}
+								/>
+							</>
+						)}
+					/>
+					<Button
+						size="large"
+						variant="contained"
+						fullWidth
+						className="mt-5 bg-slate-600 disabled:bg-slate-300"
+						type="submit"
+						disabled={!isDirty}
+					>
+						Save Changes
+					</Button>
+				</form>
+				<Divider />
+				<p className="py-2 text-xl font-semibold text-red-500">Danger Zone</p>
+				<div className="grid grid-cols-3 border border-primary-100 p-3 rounded-md gap-4 ">
+					{data?.deleted_at ? (
+						<>
+							<div className="col-span-2">
+								<p className=" text-md font-semibold">Restore User</p>
+								<p className="text-xs text-text-light">
+									Restoring a user involves reinstating their previously deleted
+									or suspended account, and allowing them to regain access and
+									functionality."
+								</p>
+							</div>
+							<div>
+								<Popconfirm
+									title="Restore User?"
+									description="Are you sure to restore the user?"
+									onConfirm={() => onDelete(null, true)}
+									okButtonProps={{
+										type: "primary",
+										style: {
+											backgroundColor: "#EF4444",
+											borderColor: "#EF4444",
+										},
+										loading: isDeleteLoading,
+									}}
+									cancelButtonProps={{
+										type: "dashed",
+									}}
+								>
+									<AntButton
+										className="float-right"
+										type="dashed"
+										// disabled={isSubmitting}
+									>
+										Restore
+									</AntButton>
+								</Popconfirm>
+							</div>
+						</>
+					) : (
+						<>
+							{data?.is_active ? (
+								<>
+									<div className="col-span-2">
+										<p className=" text-md font-semibold">Suspend User</p>
+										<p className="text-xs text-text-light">
+											Suspending a user involves temporarily restricting their
+											access to an online platform.
+										</p>
+									</div>
+									<div>
+										<Popconfirm
+											title="Suspend User?"
+											description="Are you sure to suspend user?"
+											onConfirm={() => onSuspend()}
+											okButtonProps={{
+												type: "primary",
+												style: {
+													backgroundColor: "#EF4444",
+													borderColor: "#EF4444",
+												},
+												loading: isSuspendLoading,
+											}}
+											cancelButtonProps={{
+												type: "dashed",
+											}}
+										>
+											<AntButton
+												className="float-right"
+												type="dashed"
+												// disabled={isSubmitting}
+											>
+												Suspend
+											</AntButton>
+										</Popconfirm>
+									</div>
+								</>
+							) : (
+								<>
+									<div className="col-span-2">
+										<p className=" text-md font-semibold">Activate User</p>
+										<p className="text-xs text-text-light">
+											Activating user will activate the temporarily
+											restricted/suspended user.
+										</p>
+									</div>
+									<div>
+										<Popconfirm
+											title="Activate User?"
+											description="Are you sure to activate user?"
+											onConfirm={() => onSuspend()}
+											okButtonProps={{
+												type: "primary",
+												style: {
+													backgroundColor: "#EF4444",
+													borderColor: "#EF4444",
+												},
+												loading: isSuspendLoading,
+											}}
+											cancelButtonProps={{
+												type: "dashed",
+											}}
+										>
+											<AntButton
+												className="float-right"
+												type="dashed"
+												// disabled={isSubmitting}
+											>
+												Activate
+											</AntButton>
+										</Popconfirm>
+									</div>
+								</>
+							)}
+							<div className="col-span-2">
+								<p className=" text-md font-semibold">Delete User</p>
+								<p className="text-xs text-text-light">
+									Deleting a user involves temporarily removing their account
+									and associated data.
+								</p>
+							</div>
+							<div>
+								<Popconfirm
+									title="Delete User?"
+									description="Are you sure to delete this user?"
+									onConfirm={() => onDelete()}
+									okButtonProps={{
+										type: "primary",
+										style: {
+											backgroundColor: "#EF4444",
+											borderColor: "#EF4444",
+										},
+										loading: isDeleteLoading,
+									}}
+									cancelButtonProps={{
+										type: "dashed",
+									}}
+								>
+									<AntButton
+										className="float-right text-red-500 border-red-500"
+										type="dashed"
+										// disabled={isSubmitting}
+									>
+										Delete
+									</AntButton>
+								</Popconfirm>
+							</div>
+						</>
+					)}
+
+					<div className="col-span-2">
+						<p className=" text-md font-semibold">Permanently Delete</p>
+						<p className="text-xs text-text-light">
+							Deleting a user involves permanently removing their account and
+							associated data. You will not be able to recover this user.
+						</p>
+					</div>
+					<div>
+						<Popconfirm
+							title="Delete User Permanently?"
+							description="Are you sure to delete permanently?"
+							onConfirm={() => onDelete(true)}
+							okButtonProps={{
+								type: "primary",
+								style: {
+									backgroundColor: "#EF4444",
+									borderColor: "#EF4444",
+								},
+								loading: isDeleteLoading,
+							}}
+							cancelButtonProps={{
+								type: "dashed",
+							}}
+						>
+							<AntButton
+								className="float-right text-red-500 border-red-500"
+								type="dashed"
+								// disabled={isSubmitting}
+							>
+								Delete
+							</AntButton>
+						</Popconfirm>
+					</div>
+				</div>
+			</div>
+		</Spin>
+	);
 };
 
 export default Security;
--- src/queries/users/index.ts
+++ src/queries/users/index.ts
@@ -1,26 +1,54 @@
 import instance from "@/services";
 import { IUserId } from "@/types";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 const getUsers = (params: any) => {
-  return instance.get(`/users`, {
-    params,
-  });
+	return instance.get(`/users`, {
+		params,
+	});
 };
 
 export const useGetUsers = (params: any) => {
-  return useQuery(["/users", params], () => getUsers(params));
+	return useQuery(["/users", params], () => getUsers(params));
 };
 
 const getUsersById = (id?: IUserId) => {
-  return instance.get(`/users/${id}`);
+	return instance.get(`/users/${id}`);
 };
 
 export const useGetUsersById = (id?: IUserId) => {
-  return useQuery(["/users/:id", id], () => getUsersById(id), {
-    enabled: !!id,
-    select(data) {
-      return data.data.data;
-    },
-  });
+	return useQuery(["/users/:id", id], () => getUsersById(id), {
+		enabled: !!id,
+		select(data) {
+			return data.data.data;
+		},
+	});
+};
+
+const deleteUser = ({ id, params }: { id: IUserId; params?: any }) => {
+	return instance.delete(`/users/${id}`, { params });
+};
+
+export const useDeleteUser = () => {
+	const query = useQueryClient();
+	return useMutation(deleteUser, {
+		onSuccess: () => {
+			query.invalidateQueries(["/users"]);
+			query.invalidateQueries(["/users/:id"]);
+		},
+	});
+};
+
+const suspendUser = ({ id, params }: { id: IUserId; params?: any }) => {
+	return instance.put(`/users/${id}`, { params });
+};
+
+export const useSuspendUser = () => {
+	const query = useQueryClient();
+	return useMutation(suspendUser, {
+		onSuccess: () => {
+			query.invalidateQueries(["/users"]);
+			query.invalidateQueries(["/users/:id"]);
+		},
+	});
 };
