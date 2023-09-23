ce06aaef907f377cf51d53a36667e3815769f921
Asatter833 <asatter833@gmail.com>
2023-09-23 07:46:25 +0600

    TA-9 User session completed

--- src/pages/Users/pages/Info/routes/Routes.ts
+++ src/pages/Users/pages/Info/routes/Routes.ts
@@ -2,14 +2,18 @@ import { lazy } from "react";
 import { ROUTES } from "./path";
 
 const Routes = [
-	{
-		path: ROUTES.DETAILS,
-		Component: lazy(() => import("../pages/Details")),
-	},
-	{
-		path: ROUTES.NOTFOUND,
-		Component: lazy(() => import("@/pages/NotFound")),
-	},
+  {
+    path: ROUTES.DETAILS,
+    Component: lazy(() => import("../pages/Details")),
+  },
+  {
+    path: ROUTES.SESSION,
+    Component: lazy(() => import("../pages/Session")),
+  },
+  {
+    path: ROUTES.NOTFOUND,
+    Component: lazy(() => import("@/pages/NotFound")),
+  },
 ];
 
 export default Routes;
--- src/queries/sessions/index.ts
+++ src/queries/sessions/index.ts
@@ -2,27 +2,51 @@ import instance from "@/services";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 const getEmployeeSessions = (params: any) => {
-	return instance.get(`/sessions/employees`, {
-		params,
-	});
+  return instance.get(`/sessions/employees`, {
+    params,
+  });
 };
 
 export const useGetEmployeeSessions = (params: any) => {
-	return useQuery(["/sessions/employees", params], () =>
-		getEmployeeSessions(params)
-	);
+  return useQuery(["/sessions/employees", params], () =>
+    getEmployeeSessions(params)
+  );
 };
 
 const employeeSessionSignOut = (id: number) => {
-	return instance.put(`/sessions/employees/${id}`);
+  return instance.put(`/sessions/employees/${id}`);
 };
 
 export const useEmployeeSessionSignOut = () => {
-	const query = useQueryClient();
-	return useMutation(employeeSessionSignOut, {
-		onSuccess: () => {
-			query.invalidateQueries(["/sessions/employees"]);
-			query.invalidateQueries(["/admin/validate"]);
-		},
-	});
+  const query = useQueryClient();
+  return useMutation(employeeSessionSignOut, {
+    onSuccess: () => {
+      query.invalidateQueries(["/sessions/employees"]);
+      query.invalidateQueries(["/admin/validate"]);
+    },
+  });
+};
+
+const getUserSessions = (params: any) => {
+  return instance.get(`/sessions/users`, {
+    params,
+  });
+};
+
+export const useGetUserSessions = (params: any) => {
+  return useQuery(["/sessions/users", params], () => getUserSessions(params));
+};
+
+const userSessionSignOut = (id: number) => {
+  return instance.put(`/sessions/users/${id}`);
+};
+
+export const useUserSessionSignOut = () => {
+  const query = useQueryClient();
+  return useMutation(userSessionSignOut, {
+    onSuccess: () => {
+      query.invalidateQueries(["/sessions/users"]);
+      query.invalidateQueries(["/admin/validate"]);
+    },
+  });
 };
