d91d196a8a34b6b91c64775877eb6e87cffc76f7
Asatter833 <asatter833@gmail.com>
2023-09-23 18:48:10 +0600

    create complete

--- src/pages/QsnBank/pages/Create/index.tsx
+++ src/pages/QsnBank/pages/Create/index.tsx
@@ -9,20 +9,20 @@ import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import Iconify from "@components/iconify";
 import useChapter from "@/hooks/useChapter";
+import { useCreateQsnBank } from "@/queries/qsnbank";
 
 const Create: React.FC = () => {
-  // Get Subject
-  const { chapter, isChapterLoading, searchChapter } = useChapter();
   const { handleSubmit, control, reset } = useForm({
     // resolver: joiResolver(loginResolver),
   });
-  const { mutateAsync: create, isLoading: topicCreating } = useCreateTopics();
+  const { mutateAsync: create, isLoading: qsnBankCreating } =
+    useCreateQsnBank();
 
   // On Submit Function
   const onSubmit = async (data: any) => {
     message.open({
       type: "loading",
-      content: "Creating Topic..",
+      content: "Creating Question Bank..",
       duration: 0,
     });
     const res = await handleResponse(
@@ -44,17 +44,17 @@ const Create: React.FC = () => {
   return (
     <div>
       <div className="max-w-md mt-6 mx-auto text-center">
-        <p className="text-lg font-medium mb-2">Create New Topic</p>
+        <p className="text-lg font-medium mb-2">Create New Question Bank</p>
         <p className="text-sm text-text-light">
-          Initiate a new topic creation process. Input topic title, content, and
-          preferences efficiently, empowering you to curate and organize your
-          content effectively on this page.
+          Begin your Question Bank creation here. Input questions, answers, and
+          categorization effortlessly, simplifying the management and retrieval
+          of your assessment materials on this dedicated page
         </p>
         <Link
-          to={"/app/topics/list"}
+          to={"/app/qb/list"}
           className="text-sm font-medium text-text underline"
         >
-          <p className="mt-3">View All Topics</p>
+          <p className="mt-3">View All Question Bank</p>
         </Link>
         <Divider />
       </div>
@@ -63,31 +63,6 @@ const Create: React.FC = () => {
         className="max-w-xl mb-4 mx-auto flex flex-col gap-2"
       >
         <div className="flex flex-col gap-2 border p-3 rounded-md bg-slate-50">
-          <div>
-            <Label className="my-1">Chapter</Label>
-            <Controller
-              control={control}
-              name={"chapter_id"}
-              render={({
-                field: { onChange, onBlur, value },
-                fieldState: { error },
-              }) => (
-                <Select
-                  value={value}
-                  size="large"
-                  showSearch
-                  className="w-full"
-                  placeholder={"Select a Chapter..."}
-                  suffixIcon={<Iconify icon={"mingcute:search-3-line"} />}
-                  onChange={onChange}
-                  options={chapter}
-                  onSearch={searchChapter}
-                  loading={isChapterLoading}
-                  status={error ? "error" : ""}
-                />
-              )}
-            />
-          </div>
           <Label isRequired>Name</Label>
           <Controller
             control={control}
@@ -138,7 +113,7 @@ const Create: React.FC = () => {
           size="large"
           type={"submit"}
           className="w-full mt-4"
-          disabled={topicCreating}
+          disabled={qsnBankCreating}
         >
           Submit
         </Button>
