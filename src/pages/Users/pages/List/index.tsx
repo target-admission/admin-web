f232ced96dcb265c40c2b179a53402904a6f37f2
Asatter833 <asatter833@gmail.com>
2023-09-23 06:26:44 +0600

    TA-9 Data Showed in the ui

--- src/pages/Users/pages/List/index.tsx
+++ src/pages/Users/pages/List/index.tsx
@@ -5,14 +5,26 @@ import React from "react";
 import UserColumn from "./components/userColumn";
 
 const List: React.FC = () => {
-  const { getQueryParams } = useQueryContext();
+  const { getQueryParams, page } = useQueryContext();
 
-  const { data } = useGetUsers({ getQueryParams });
+  const { data, isLoading } = useGetUsers({ getQueryParams });
   console.log(data);
 
   return (
     <div className="p-3">
-      <DataGrid columns={UserColumn()} rows={data?.data?.data || []} />
+      <DataGrid
+        columns={UserColumn()}
+        rows={data?.data?.data || []}
+        loading={isLoading}
+        getRowId={(r: any) => r?.id}
+        rowCount={data?.data?.total || 0}
+        initialState={{
+          pagination: { paginationModel: { pageSize: 5, page: page } },
+        }}
+        pageSizeOptions={[5, 10, 25, 50, 100]}
+        paginationMode={"server"}
+        disableRowSelectionOnClick
+      />
     </div>
   );
 };
